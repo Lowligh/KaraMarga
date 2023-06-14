6ba83b7a09e1e10fa3ea7d8815e37fc10d7e8688
Стас Плиев <wightondb@gmail.com>
2023-06-14 11:33:53 +0300

    Добавьте файлы проекта.

---
+++ karas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace karas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
